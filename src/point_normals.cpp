4d0a206f8649c3907bb9e00e4cb45046eb8e82c8
joker <a2267964591>
2019-09-27 23:42:05 +0800

    实验

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -1,18 +1,22 @@
 #include <pcl/point_types.h>
 #include <pcl/features/normal_3d.h>
+#include <pcl/io/pcd_io.h>
 
 int main()
 
 {
-    pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
 
+    pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/joker/point_cloud/1.pcd", *cloud);
     // Create the normal estimation class, and pass the input dataset to it
-    pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> ne;
+    pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
+
     ne.setInputCloud(cloud);
 
     // Create an empty kdtree representation, and pass it to the normal estimation object.
     // Its content will be filled inside the object, based on the given input dataset (as no other search surface is given).
-    pcl::search::KdTree<pcl::PointXYZ>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZ>());
+    pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>());
+
     ne.setSearchMethod(tree);
 
     // Output datasets
@@ -21,7 +25,7 @@ int main()
     // Use all neighbors in a sphere of radius 3cm
     ne.setRadiusSearch(0.03);
 
-    // Compute the features
+    // Compute the features 
     ne.compute(*cloud_normals);
 
     // cloud_normals->points.size () should have the same size as the input cloud->points.size ()
