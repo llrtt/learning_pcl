5ae99df966296b3f2906a2501a95b784cfead8db
joker <a2267964591>
2019-09-28 21:14:12 +0800

    会动的点云

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -12,26 +12,57 @@ int main()
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
   pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
 
-  uint8_t r(255), g(15), b(15);
-  for (float z(-1.0); z <= 4.0; z += 0.01)
-  {
-    for (float angle(0.0); angle <= 360.0; angle += 5.0)
-    {
-      pcl::PointXYZ basic_point;
-      basic_point.x = 0.5 * std::cos(pcl::deg2rad(angle));
-      basic_point.y = sinf(pcl::deg2rad(angle));
-      basic_point.z = z;
-      basic_cloud_ptr->points.push_back(basic_point);
+  // uint8_t r(255), g(15), b(15);
+  // for (float z(-1.0); z <= 4.0; z += 0.01)
+  // {
+  //   for (float angle(0.0); angle <= 360.0; angle += 5.0)
+  //   {
+  //     pcl::PointXYZ basic_point;
+  //     basic_point.x = 0.5 * std::cos(pcl::deg2rad(angle));
+  //     basic_point.y = sinf(pcl::deg2rad(angle));
+  //     basic_point.z = z;
+  //     basic_cloud_ptr->points.push_back(basic_point);
 
-      pcl::PointXYZRGB point;
-      point.x = basic_point.x;
-      point.y = basic_point.y;
-      point.z = basic_point.z;
-      uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
-                      static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
-      point.rgb = *reinterpret_cast<float *>(&rgb);
-      point_cloud_ptr->points.push_back(point);
-    }
+  //     pcl::PointXYZRGB point;
+  //     point.x = basic_point.x;
+  //     point.y = basic_point.y;
+  //     point.z = basic_point.z;
+  //     uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
+  //                     static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
+  //     point.rgb = *reinterpret_cast<float *>(&rgb);
+  //     point_cloud_ptr->points.push_back(point);
+  //   }
+  //   if (z < 0.0)
+  //   {
+  //     r -= 12;
+  //     g += 12;
+  //   }
+  //   else
+  //   {
+  //     g -= 12;
+  //     b += 12;
+  //   }
+  // }
+
+  uint8_t r(255), g(15), b(15);
+  float i = 0;
+  for (float z = -10; z <= 10; z += 0.01)
+  {
+    i++;
+    pcl::PointXYZ basic_point;
+    basic_point.x = std::cos(0.1 * i);
+    basic_point.y = sinf(0.1 * i);
+    basic_point.z = z;
+    basic_cloud_ptr->points.push_back(basic_point);
+
+    pcl::PointXYZRGB point;
+    point.x = basic_point.x;
+    point.y = basic_point.y;
+    point.z = basic_point.z;
+    uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
+                    static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
+    point.rgb = *reinterpret_cast<float *>(&rgb);
+    point_cloud_ptr->points.push_back(point);
     if (z < 0.0)
     {
       r -= 12;
@@ -49,19 +80,58 @@ int main()
   point_cloud_ptr->width = (int)point_cloud_ptr->points.size();
   point_cloud_ptr->height = 1;
 
-  pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
-  viewer->setBackgroundColor (0, 0, 0);
+  pcl::visualization::PCLVisualizer::Ptr viewer(new pcl::visualization::PCLVisualizer("3D Viewer"));
+  viewer->setBackgroundColor(0, 0, 0);
   pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(point_cloud_ptr);
-  viewer->addPointCloud<pcl::PointXYZRGB> (point_cloud_ptr,rgb, "sample cloud");
-  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
-  viewer->addCoordinateSystem (1.0);
-  viewer->initCameraParameters ();
+  viewer->addPointCloud<pcl::PointXYZRGB>(point_cloud_ptr, rgb, "sample cloud");
+  viewer->setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+  viewer->addCoordinateSystem(1.0);
+  viewer->initCameraParameters();
 
-
+  float w = 0.1;
   while (!viewer->wasStopped())
   {
+    point_cloud_ptr->clear();
+    w+=0.1;
+    if (w >= 3.141592 * 2)
+    {
+      w = 0.1;
+    }
+    uint8_t r(255), g(15), b(15);
+    float i = 0;
+    for (float z = -10; z <= 10; z += 0.01)
+    {
+      i++;
+      pcl::PointXYZ basic_point;
+      basic_point.x = std::cos(0.1 * i + w);
+      basic_point.y = sinf(0.1 * i + w);
+      basic_point.z = z;
+      basic_cloud_ptr->points.push_back(basic_point);
+
+      pcl::PointXYZRGB point;
+      point.x = basic_point.x;
+      point.y = basic_point.y;
+      point.z = basic_point.z;
+      uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
+                      static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
+      point.rgb = *reinterpret_cast<float *>(&rgb);
+      point_cloud_ptr->points.push_back(point);
+      if (z < 0.0)
+      {
+        r -= 12;
+        g += 12;
+      }
+      else
+      {
+        g -= 12;
+        b += 12;
+      }
+    }
+    pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(point_cloud_ptr);
+    viewer->updatePointCloud<pcl::PointXYZRGB>(point_cloud_ptr, rgb, "sample cloud");
     viewer->spinOnce(100);
+    
   }
 
-    return 0;
-  }
+  return 0;
+}
