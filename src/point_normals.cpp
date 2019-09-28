aa6b2db60c600543c7f99e04ca97b8ac6bd6d047
joker <a2267964591>
2019-09-28 13:53:16 +0800

    花里胡哨的点云

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -1,61 +1,69 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <iostream>
-#include <pcl/io/io.h>
+#include <thread>
+
+#include <pcl/common/common_headers.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
-    
-int user_data;
-    
-void 
-viewerOneOff (pcl::visualization::PCLVisualizer& viewer)
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/console/parse.h>
+
+int main()
 {
-    viewer.setBackgroundColor (1.0, 0.5, 1.0);
-    pcl::PointXYZ o;
-    o.x = 1.0;
-    o.y = 0;
-    o.z = 0;
-    viewer.addSphere (o, 0.25, "sphere", 0);
-    std::cout << "i only run once" << std::endl;
-    
-}
-    
-void 
-viewerPsycho (pcl::visualization::PCLVisualizer& viewer)
-{
-    static unsigned count = 0;
-    std::stringstream ss;
-    ss << "Once per viewer loop: " << count++;
-    viewer.removeShape ("text", 0);
-    viewer.addText (ss.str(), 200, 300, "text", 0);
-    
-    //FIXME: possible race condition here:
-    user_data++;
-}
-    
-int 
-main ()
-{
-    pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGBA>);
-    pcl::io::loadPCDFile ("my_point_cloud.pcd", *cloud);
-    
-    pcl::visualization::CloudViewer viewer("Cloud Viewer");
-    
-    //blocks until the cloud is actually rendered
-    viewer.showCloud(cloud);
-    
-    //use the following functions to get access to the underlying more advanced/powerful
-    //PCLVisualizer
-    
-    //This will only get called once
-    viewer.runOnVisualizationThreadOnce (viewerOneOff);
-    
-    //This will get called once per visualization iteration
-    viewer.runOnVisualizationThread (viewerPsycho);
-    while (!viewer.wasStopped ())
+  pcl::PointCloud<pcl::PointXYZRGBA>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGBA>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr point_cloud_ptr(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr basic_cloud_ptr(new pcl::PointCloud<pcl::PointXYZ>);
+
+  uint8_t r(255), g(15), b(15);
+  for (float z(-1.0); z <= 1.0; z += 0.05)
+  {
+    for (float angle(0.0); angle <= 360.0; angle += 5.0)
     {
-    //you can also do cool processing here
-    //FIXME: Note that this is running in a separate thread from viewerPsycho
-    //and you should guard against race conditions yourself...
-    user_data++;
+      pcl::PointXYZ basic_point;
+      basic_point.x = 0.5 * std::cos(pcl::deg2rad(angle));
+      basic_point.y = sinf(pcl::deg2rad(angle));
+      basic_point.z = z;
+      basic_cloud_ptr->points.push_back(basic_point);
+
+      pcl::PointXYZRGB point;
+      point.x = basic_point.x;
+      point.y = basic_point.y;
+      point.z = basic_point.z;
+      uint32_t rgb = (static_cast<uint32_t>(r) << 16 |
+                      static_cast<uint32_t>(g) << 8 | static_cast<uint32_t>(b));
+      point.rgb = *reinterpret_cast<float *>(&rgb);
+      point_cloud_ptr->points.push_back(point);
     }
+    if (z < 0.0)
+    {
+      r -= 12;
+      g += 12;
+    }
+    else
+    {
+      g -= 12;
+      b += 12;
+    }
+  }
+
+  basic_cloud_ptr->width = (int)basic_cloud_ptr->points.size();
+  basic_cloud_ptr->height = 1;
+  point_cloud_ptr->width = (int)point_cloud_ptr->points.size();
+  point_cloud_ptr->height = 1;
+
+  pcl::visualization::PCLVisualizer::Ptr viewer (new pcl::visualization::PCLVisualizer ("3D Viewer"));
+  viewer->setBackgroundColor (0, 0, 0);
+  pcl::visualization::PointCloudColorHandlerRGBField<pcl::PointXYZRGB> rgb(point_cloud_ptr);
+  viewer->addPointCloud<pcl::PointXYZRGB> (point_cloud_ptr,rgb, "sample cloud");
+  viewer->setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "sample cloud");
+  viewer->addCoordinateSystem (1.0);
+  viewer->initCameraParameters ();
+
+
+  while (!viewer->wasStopped())
+  {
+    viewer->spinOnce(100);
+  }
+
     return 0;
-}
+  }
