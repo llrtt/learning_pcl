f879934f9b176bdf440600ddf84c1d7a5d91aea5
joker <a2267964591>
2019-09-29 12:56:04 +0800

    region_growing_segmentation

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -1,39 +1,73 @@
+#include <iostream>
+#include <vector>
 #include <pcl/point_types.h>
-#include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
+#include <pcl/search/search.h>
+#include <pcl/search/kdtree.h>
+#include <pcl/features/normal_3d.h>
 #include <pcl/visualization/cloud_viewer.h>
-#include <pcl/filters/voxel_grid.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing.h>
 
-int main(int argc, char **argv)
-
+int
+main (int argc, char** argv)
 {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr Ammunition_box(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZ>);
+  if ( pcl::io::loadPCDFile <pcl::PointXYZ> ("/home/joker/Desktop/point_cloud/1.pcd", *cloud) == -1)
+  {
+    std::cout << "Cloud reading failed." << std::endl;
+    return (-1);
+  }
 
-  pcl::io::loadPCDFile<pcl::PointXYZRGB>("/home/joker/Desktop/point_cloud/1.pcd", *Ammunition_box);
+  pcl::search::Search<pcl::PointXYZ>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZ>);
+  pcl::PointCloud <pcl::Normal>::Ptr normals (new pcl::PointCloud <pcl::Normal>);
+  pcl::NormalEstimation<pcl::PointXYZ, pcl::Normal> normal_estimator;
+  normal_estimator.setSearchMethod (tree);
+  normal_estimator.setInputCloud (cloud);
+  normal_estimator.setKSearch (50);
+  normal_estimator.compute (*normals);
 
-  pcl::visualization::PCLVisualizer viewer("Ammunition_box");
+  pcl::IndicesPtr indices (new std::vector <int>);
+  pcl::PassThrough<pcl::PointXYZ> pass;
+  pass.setInputCloud (cloud);
+  pass.setFilterFieldName ("z");
+  pass.setFilterLimits (0.0, 1.0);
+  pass.filter (*indices);
 
-  pcl::VoxelGrid<pcl::PointXYZRGB> v;
+  pcl::RegionGrowing<pcl::PointXYZ, pcl::Normal> reg;
+  reg.setMinClusterSize (50);
+  reg.setMaxClusterSize (1000000);
+  reg.setSearchMethod (tree);
+  reg.setNumberOfNeighbours (30);
+  reg.setInputCloud (cloud);
+  //reg.setIndices (indices);
+  reg.setInputNormals (normals);
+  reg.setSmoothnessThreshold (3.0 / 180.0 * M_PI);
+  reg.setCurvatureThreshold (1.0);
 
-  v.setInputCloud(Ammunition_box);
+  std::vector <pcl::PointIndices> clusters;
+  reg.extract (clusters);
 
-  v.setLeafSize(0.006f, 0.006f, 0.006f);
-  
-  v.filter(*Ammunition_box);
+  std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
+  std::cout << "First cluster has " << clusters[0].indices.size () << " points." << std::endl;
+  std::cout << "These are the indices of the points of the initial" <<
+    std::endl << "cloud that belong to the first cluster:" << std::endl;
+  int counter = 0;
+  while (counter < clusters[0].indices.size ())
+  {
+    std::cout << clusters[0].indices[counter] << ", ";
+    counter++;
+    if (counter % 10 == 0)
+      std::cout << std::endl;
+  }
+  std::cout << std::endl;
 
-  viewer.setCameraPosition(0, 0, -3.0, 0, -1, 0);
+  pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
+  pcl::visualization::CloudViewer viewer ("Cluster viewer");
+  viewer.showCloud(colored_cloud);
+  while (!viewer.wasStopped ())
+  {
+  }
 
-  viewer.addPointCloud(Ammunition_box, "Ammunition_box");
-
-  viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Ammunition_box");
-
-  viewer.addCoordinateSystem(0.3);
-
-  viewer.initCameraParameters();
-
-  while (!viewer.wasStopped())
-
-    viewer.spinOnce(100);
-
-  return 0;
+  return (0);
 }
