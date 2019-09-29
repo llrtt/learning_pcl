173424f8c7a5f123f6e12db535d318723ecdd9da
joker <a2267964591>
2019-09-29 12:39:29 +0800

    向下取样

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -2,30 +2,38 @@
 #include <pcl/features/normal_3d.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/voxel_grid.h>
 
-int main(int argc, char** argv)
+int main(int argc, char **argv)
 
 {
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr Ammunition_box(new pcl::PointCloud<pcl::PointXYZRGB>);
 
-  pcl::io::loadPCDFile<pcl::PointXYZRGB> ("/home/joker/Desktop/point_cloud/1.pcd", *Ammunition_box);
+  pcl::io::loadPCDFile<pcl::PointXYZRGB>("/home/joker/Desktop/point_cloud/1.pcd", *Ammunition_box);
 
   pcl::visualization::PCLVisualizer viewer("Ammunition_box");
 
-  viewer.setCameraPosition(0,0,-3.0,0,-1,0);
+  pcl::VoxelGrid<pcl::PointXYZRGB> v;
+
+  v.setInputCloud(Ammunition_box);
+
+  v.setLeafSize(0.006f, 0.006f, 0.006f);
+  
+  v.filter(*Ammunition_box);
+
+  viewer.setCameraPosition(0, 0, -3.0, 0, -1, 0);
 
   viewer.addPointCloud(Ammunition_box, "Ammunition_box");
 
-  viewer.setPointCloudRenderingProperties (pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Ammunition_box");
+  viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "Ammunition_box");
 
-  viewer.addCoordinateSystem (0.3);
+  viewer.addCoordinateSystem(0.3);
 
-  viewer.initCameraParameters ();
+  viewer.initCameraParameters();
 
-  while(!viewer.wasStopped())
+  while (!viewer.wasStopped())
 
-  viewer.spinOnce(100); 
+    viewer.spinOnce(100);
 
   return 0;
-
 }
