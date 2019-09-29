29a8e3abdb5a8d0398a7b48c3086fbf86a7dbca4
joker <a2267964591>
2019-09-29 21:45:33 +0800

    points_normals

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -9,70 +9,35 @@
 #include <pcl/filters/passthrough.h>
 #include <pcl/segmentation/region_growing.h>
 
-int
-main (int argc, char** argv)
+int main(int argc, char **argv)
 {
-  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud (new pcl::PointCloud<pcl::PointXYZRGB>);
-  if ( pcl::io::loadPCDFile <pcl::PointXYZRGB> ("/home/joker/Desktop/point_cloud/1.pcd", *cloud) == -1)
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  if (pcl::io::loadPCDFile<pcl::PointXYZRGB>("/home/joker/Desktop/point_cloud/1.pcd", *cloud) == -1)
   {
     std::cout << "Cloud reading failed." << std::endl;
     return (-1);
   }
 
-  pcl::search::Search<pcl::PointXYZRGB>::Ptr tree (new pcl::search::KdTree<pcl::PointXYZRGB>);
-  pcl::PointCloud <pcl::Normal>::Ptr normals (new pcl::PointCloud <pcl::Normal>);
-  pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> normal_estimator;
-  normal_estimator.setSearchMethod (tree);
-  normal_estimator.setInputCloud (cloud);
-  normal_estimator.setKSearch (50);
-  normal_estimator.compute (*normals);
+  pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
+  ne.setInputCloud(cloud);
+  ne.setSearchSurface(cloud);
 
-  pcl::IndicesPtr indices (new std::vector <int>);
-  pcl::PassThrough<pcl::PointXYZRGB> pass;
-  pass.setInputCloud (cloud);
-  pass.setFilterFieldName ("z");
-  pass.setFilterLimits (0.0, 1.0);
-  pass.filter (*indices);
+  pcl::search::KdTree<pcl::PointXYZRGB>::Ptr tree(new pcl::search::KdTree<pcl::PointXYZRGB>);
+  ne.setSearchMethod(tree);
+  pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
 
-  pcl::RegionGrowing<pcl::PointXYZRGB, pcl::Normal> reg;
-  reg.setMinClusterSize (50);
-  reg.setMaxClusterSize (1000000);
-  reg.setSearchMethod (tree);
-  reg.setNumberOfNeighbours (30);
-  reg.setInputCloud (cloud);
-  //reg.setIndices (indices);
-  reg.setInputNormals (normals);
-  reg.setSmoothnessThreshold (3.0 / 180.0 * M_PI);
-  reg.setCurvatureThreshold (1.0);
+  ne.setRadiusSearch(0.03);
+  ne.compute(*cloud_normals);
 
-  std::vector <pcl::PointIndices> clusters;
-  reg.extract (clusters);
-
-  std::cout << "Number of clusters is equal to " << clusters.size () << std::endl;
-  std::cout << "First cluster has " << clusters[0].indices.size () << " points." << std::endl;
-  std::cout << "These are the indices of the points of the initial" <<
-    std::endl << "cloud that belong to the first cluster:" << std::endl;
-  int counter = 0;
-  while (counter < clusters[0].indices.size ())
-  {
-    std::cout << clusters[0].indices[counter] << ", ";
-    counter++;
-    if (counter % 10 == 0)
-      std::cout << std::endl;
-  }
-  std::cout << std::endl;
-
-  pcl::PointCloud <pcl::PointXYZRGB>::Ptr colored_cloud = reg.getColoredCloud ();
-  pcl::visualization::PCLVisualizer viewer ("Cluster viewer");
-  viewer.setCameraPosition(0, 0, 0, 0, 0, 0);
+  pcl::visualization::PCLVisualizer viewer;
+  viewer.addPointCloud(cloud, "cloud");
+  viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 3);
+  viewer.addPointCloudNormals<pcl::PointXYZRGB, pcl::Normal>(cloud, cloud_normals, 10, 0.005, "normals");
   viewer.addCoordinateSystem(0.2);
-  viewer.addPointCloud(colored_cloud, "colored_cloud");
-  viewer.setPointCloudRenderingProperties(pcl::visualization::PCL_VISUALIZER_POINT_SIZE, 1, "colored_cloud");
   viewer.initCameraParameters();
-  while (!viewer.wasStopped ())
+  while(!viewer.wasStopped())
   {
     viewer.spinOnce(100);
   }
-
   return (0);
 }
