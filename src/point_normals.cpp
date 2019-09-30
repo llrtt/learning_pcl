7dcf57dddd2b883056eb14cadcf5999912542cbf
joker <a2267964591>
2019-09-30 20:11:20 +0800

    0

--- src/point_normals.cpp
+++ src/point_normals.cpp
@@ -8,15 +8,29 @@
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/filters/passthrough.h>
 #include <pcl/segmentation/region_growing.h>
+#include <pcl/filters/statistical_outlier_removal.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/surface/mls.h>
 
 int main(int argc, char **argv)
 {
   pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZRGB>);
+  pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_filtered(new pcl::PointCloud<pcl::PointXYZRGB>);
   if (pcl::io::loadPCDFile<pcl::PointXYZRGB>("/home/joker/Desktop/point_cloud/1.pcd", *cloud) == -1)
   {
     std::cout << "Cloud reading failed." << std::endl;
     return (-1);
   }
+  pcl::StatisticalOutlierRemoval<pcl::PointXYZRGB> sor;
+  sor.setInputCloud (cloud);
+  sor.setMeanK (50);
+  sor.setStddevMulThresh (1.0);
+  sor.filter (*cloud);
+
+  pcl::VoxelGrid<pcl::PointXYZRGB> v;
+  v.setInputCloud(cloud);
+  v.setLeafSize(0.006f, 0.006f, 0.006f);
+  v.filter(*cloud);
 
   pcl::NormalEstimation<pcl::PointXYZRGB, pcl::Normal> ne;
   ne.setInputCloud(cloud);
@@ -26,7 +40,7 @@ int main(int argc, char **argv)
   ne.setSearchMethod(tree);
   pcl::PointCloud<pcl::Normal>::Ptr cloud_normals(new pcl::PointCloud<pcl::Normal>);
 
-  ne.setRadiusSearch(0.03);
+  ne.setRadiusSearch(0.1);
   ne.compute(*cloud_normals);
 
   pcl::visualization::PCLVisualizer viewer;
